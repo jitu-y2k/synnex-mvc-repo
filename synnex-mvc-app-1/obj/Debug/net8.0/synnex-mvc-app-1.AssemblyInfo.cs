4fa791ff940860d87c8a297779fbeb421d248fdd
jitendra sharma  (HCL Financial Services) <jitu_y2k@yahoo.com>
2024-06-18 02:46:31 +0530

    Added Cancel button functionality to Create Student Page

--- synnex-mvc-app-1/obj/Debug/net8.0/synnex-mvc-app-1.AssemblyInfo.cs
+++ synnex-mvc-app-1/obj/Debug/net8.0/synnex-mvc-app-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("synnex-mvc-app-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63124a29ca0f47726c72b481d4b74d11cb64f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe3344b1b16c4ab423eb7f20c5ca9dfadc1cde2")]
 [assembly: System.Reflection.AssemblyProductAttribute("synnex-mvc-app-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("synnex-mvc-app-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
